327d0548e2de4a8977a1195cd2a581bc33d1bf76
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2024-02-25 08:31:31 +0100

    comment for the LazyLoadingString struct

--- include/string_repository.h
+++ include/string_repository.h
@@ -8,6 +8,23 @@
 
 extern char* __brkval;
 
+
+/**
+ * @brief Structure to manage lazy loading of strings from flash program memory
+ *
+ * FlashString holds the reference to the address of the string in the flash program memory, pointerToString holds a
+ * reference to the heap address of the string if it was already loaded into the heap, or is NULL if it wasn't loaded
+ * before.
+ *
+ * It's always necessary to use at least one variable to a string, a pointer to a string, that you will use in your
+ * program this construct add another pointer, so the use of RAM(stack of main) will increase by 2Byte for a pointer at
+ * avr-gcc compiler for every Lazy Loading String you manage here, but it will spare you space in RAM at startup for
+ * every String that is larger than this 2byte.
+ *
+ * It only uses the RAM if the String is needed, and then uses the heap. You are free to free the heap memory for this
+ * string (pointerToString) manually or add the string to the array and use the rudimentary memory management that aims
+ * as priority for a low overhead of management
+ * */
 typedef struct {
     const char* flashString;
     char* pointerToString;

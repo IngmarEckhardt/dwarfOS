9e7b4d327e49b579d944726d63b14f0d48f544bf
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2024-02-25 08:14:52 +0100

    deleted declaration of deleted strings

--- include/string_storage.h
+++ include/string_storage.h
@@ -4,9 +4,5 @@
 #include "string_repository.h"
 
 LazyLoadingString initMsg;
-LazyLoadingString cet;
-LazyLoadingString cest;
-LazyLoadingString catDetect;
-LazyLoadingString noCatDetect;
 
 #endif //DWARFOS_STRING_STORAGE_H

2b0babd1d0238ac7152f9c7b66924039b2966912
IngmarEckhardt <88197507+IngmarEckhardt@users.noreply.github.com>
2024-03-17 16:55:31 +0100

    Update pgm_textfile_generator.h

--- include/pgm_textfile_generator.h
+++ include/pgm_textfile_generator.h
@@ -3,8 +3,9 @@
 
 #include <stdint.h>
 
-// Provide a const array with non-const characters, specify the number of characters and the maximum number of bytes for each individual array, as well as the number of borders you provide.
-// This function will create arrays of const structures with a uint8_t index information, indicating the index where the string originally existed in the array, and append the string as a const char.
+// Provide a const array with non-const strings, specify the number of strings and the maximum number of strings for each individual array, as well as the number of borders you provide.
+// This function will create arrays of const structures with a uint8_t index information, indicating the index where the string originally existed in the array, and append the string as a const char
+// array.
 // The arrays must have constant-sized placeholders for the individual sized strings to fit into PROGMEM.
 // Strings that do not fit into your array sizes will be stored as single strings in PROGMEM.
 // The usage of this converter can be observed in the colossal-cave-adventure repository (located in the /test folder, with the output included in the project).

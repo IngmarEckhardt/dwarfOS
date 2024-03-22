06f9431f315c692b4fed69f186049603f0b37310
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2024-03-22 12:27:07 +0100

    fixed documentation after 1.3.1 refactoring

--- dwarf-os/flash_helper.h
+++ dwarf-os/flash_helper.h
@@ -5,6 +5,7 @@
 
 #ifndef DWARFOS_FLASH_HELPER_H
 #define DWARFOS_FLASH_HELPER_H
+
 #include <avr/pgmspace.h>
 #include <stdint.h>
 
@@ -12,22 +13,25 @@
 /**
  * @brief Structure for managing text files in extended program memory.
  *
- * FarTextFile is used to manage text files that are stored in extended program memory.
+ * TextFile is used to manage text files that are stored in program memory.
  * These files can be generated with the `pgm_textfile_generator`.
  */
 typedef struct {
-    const uint32_t pointerToArray; /**< The far pointer to the text file in extended program memory. */
+    const uint32_t pointerToArray; /**< The pointer to the text file in program memory. */
     const uint8_t amountOfEntries; /**< The number of entries in the text file. */
     const uint8_t sizeOfIndexArray; /**< The size of the index array. */
     const uint16_t maxLengthOfStrings; /**< The maximum length of the strings in the text file. */
 } TextFile;
 
 typedef struct FlashHelper FlashHelper;
+
 /**
  * @brief Structure for managing string storage and lazy loading.
  *
  * FlashHelper manages the loading of strings from flash memory to RAM.
- * It contains functions to initialize string storage and load strings from flash memory.
+ * It contains function pointers to initialize string storage and load strings from flash memory.
+ * The actual functions pointed to by these pointers can vary depending on whether the device supports
+ * Extended Linear Addressing (ELPM). This is determined at startup.
  */
 struct FlashHelper {
     /**
@@ -40,6 +44,7 @@ struct FlashHelper {
    * @return A pointer to the newly created string in RAM.
    */
     char * (* createString_P)(uint32_t flashString, FlashHelper * helper);
+
     /**
     * @brief Loads a string from program memory to a buffer in RAM.
     *
@@ -47,14 +52,39 @@ struct FlashHelper {
     * and loads the string from program memory to the buffer in RAM.
     *
     * @param stringBuffer A pointer to the buffer in RAM.
-    * @param pointerToNearProgMemString A pointer to the string in program memory.
+    * @param pointerToProgMemString A pointer to the string in program memory.
     */
     char * (* copyString_P)(char * stringBuffer, uint32_t flashString);
 
-    int16_t (*putString_P)(uint32_t farPointerToString);
+    /**
+    * @brief Writes a string from program memory to the standard output.
+    *
+    * This function takes a pointer to a string in program memory and writes the string to the standard output.
+    * The function returns 0 on success, or EOF on error.
+    *
+    * @param pointerToString A pointer to the string in program memory.
+    * @return 0 on success, or EOF on error.
+    */
+    int16_t (* putString_P)(uint32_t pointerToString);
 
+    /**
+    * @brief Returns the length of a string in program memory.
+    *
+    * This function takes a pointer to a string in program memory and returns the length of the string.
+    *
+    * @param flashString A pointer to the string in program memory.
+    * @return The length of the string.
+    */
     uint16_t (* lengthOfString_P)(uint32_t flashString);
 
+    /**
+    * @brief Reads a byte from program memory.
+    *
+    * This function takes an address in program memory and returns the byte at that address.
+    *
+    * @param address The address in program memory.
+    * @return The byte at the specified address.
+    */
     uint8_t (* readByte_P)(uint32_t address);
 
     /**
@@ -71,34 +101,46 @@ struct FlashHelper {
     int16_t (* compareString_P)(const char * string, uint32_t flashString);
 
     /**
-    * @brief Loads a string from a text file in program memory to RAM.
+    * @brief Retrieves a string from a text file in program memory and loads it into RAM.
     *
-    * This function takes a pointer to a NearTextFile structure and an index, and loads the string
-    * at the specified index from the text file in program memory to RAM. The function returns a pointer
-    * to the newly created string in RAM.
+    * This function takes a pointer to a TextFile structure and an index. If the TextFile has an index array (sizeOfIndexArray is not 0),
+    * it finds the string in the text file where the stored number in the index array matches the provided index. If the TextFile does not
+    * have an index array (sizeOfIndexArray is 0), it retrieves the string at the position corresponding to the provided index.
+    * The retrieved string is then loaded into RAM and the function returns a pointer to this newly created string.
     *
-    * @param textFile A pointer to the NearTextFile structure.
-    * @param index The index of the string in the text file.
+    * @param textFile A pointer to the TextFile structure.
+    * @param index The index of the string in the text file or the number to match in the index array.
     * @return A pointer to the newly created string in RAM.
     */
     char * (* createFileString_P)(TextFile * textFile, const uint8_t index, FlashHelper * helper);
 
-
     /**
     * @brief Writes a string from a text file in program memory to the standard output.
     *
-    * This function takes a pointer to a NearTextFile structure and an index, and writes the string
-    * at the specified index from the text file in program memory to the standard output. The function
-    * returns 0 on success, or EOF on error.
+    * This function takes a pointer to a TextFile structure and an index. If the TextFile has an index array (sizeOfIndexArray is not 0),
+    * it finds the string in the text file where the stored number in the index array matches the provided index. If the TextFile does not
+    * have an index array (sizeOfIndexArray is 0), it retrieves the string at the position corresponding to the provided index.
+    * The retrieved string is then written to the standard output. The function returns 0 on success, or EOF on error.
     *
-    * @param textFile A pointer to the NearTextFile structure.
-    * @param index The index of the string in the text file.
+    * @param textFile A pointer to the TextFile structure.
+    * @param index The index of the string in the text file or the number to match in the index array.
     * @return 0 on success, or EOF on error.
     */
     int16_t (* putFileString_P)(TextFile * textFile, const uint8_t index, FlashHelper * helper);
-
-//if desired String in Ram is 1 the function returns the string, otherwise it puts the string into stdout
-    char * (*getOrPutDosMessage)(uint8_t message, uint8_t desiredStringInRam, FlashHelper * helper);
+    /**
+    * @brief Retrieves or prints a specific message from program memory.
+    *
+    * This function takes a message identifier, a flag indicating whether the string should be returned or printed,
+    * and a pointer to a FlashHelper structure. If the flag is set (1), the function retrieves the corresponding
+    * message from program memory and returns it as a string in RAM. If the flag is not set (0), the function prints
+    * the corresponding message from program memory to the standard output.
+    *
+    * @param message An identifier for the message to be retrieved or printed. This corresponds to the macros defined at the end of the file.
+    * @param desiredStringInRam A flag indicating whether the message should be returned as a string in RAM (1) or printed to the standard output (0).
+    * @param helper A pointer to the FlashHelper structure.
+    * @return If desiredStringInRam is set, a pointer to the newly created string in RAM. If desiredStringInRam is not set, NULL.
+    */
+    char * (* getOrPutDosMessage)(uint8_t message, uint8_t desiredStringInRam, FlashHelper * helper);
 };
 
 /**
@@ -114,10 +156,34 @@ struct FlashHelper {
 FlashHelper * dOS_initFlashHelper(uint8_t desiredState);
 
 #ifdef __AVR_HAVE_ELPM__
+/**
+ * @brief Macro to get the address of a string in program memory.
+ *
+ * This macro takes a string in program memory and returns its address. The address is returned as a 32-bit integer.
+ * The macro uses different methods to get the address depending on whether the device supports Extended Linear Addressing (ELPM).
+ *
+ * @param flashString The string in program memory.
+ * @return The address of the string.
+ */
 #define addressOf(flashString) pgm_get_far_address(flashString)
 #else
+/**
+ * @brief Macro to get the address of a string in program memory.
+ *
+ * This macro takes a string in program memory and returns its address. The address is returned as a 32-bit integer.
+ * The macro uses different methods to get the address depending on whether the device supports Extended Linear Addressing (ELPM).
+ *
+ * @param flashString The string in program memory.
+ * @return The address of the string.
+ */
 #define addressOf(flashString) ((uint32_t) &flashString)
 #endif
+
+/*
+ * @brief Macros for identifying specific strings in program memory.
+ *
+ * These macros are used to identify specific strings in a program memory array. The strings are used for various purposes in the program.
+ */
 
 #define IDENT_STRING 0
 #define FREE_MEMORY_STRING 1
--- dwarf-os/pgm_textfile_generator.h
+++ dwarf-os/pgm_textfile_generator.h
@@ -3,13 +3,28 @@
 
 #include <stdint.h>
 
-// Provide a const array with non-const strings, specify the number of strings and the maximum number of strings for each individual array, as well as the number of borders you provide.
-// This function will create arrays of const structures with a uint8_t index information, indicating the index where the string originally existed in the array, and append the string as a const char
-// array.
-// The arrays must have constant-sized placeholders for the individual sized strings to fit into PROGMEM.
-// Strings that do not fit into your array sizes will be stored as single strings in PROGMEM.
-// The usage of this converter can be observed in the colossal-cave-adventure repository (located in the /test folder, with the output included in the project).
-void convertStringsToPGMTextFile(const char ** arrayWithStrings, uint16_t amountOfStrings, const uint16_t maxSizesOfStringsInPGMArrays[],
-                                 uint8_t desiredAmountPGMArrays, char * desiredNamingAllUpperCase, uint8_t desiredWithIndexArray);
+/**
+ * @brief Converts an array of strings into program memory text files.
+ *
+ * This function takes an array of strings and creates arrays of constant structures in program memory.
+ * Each structure contains a uint8_t index information, indicating the original index of the string in the array,
+ * and a string as a constant character array.
+ *
+ * The arrays must have constant-sized placeholders for the individual sized strings to fit into PROGMEM.
+ * Strings that do not fit into your array sizes will be stored as single strings in PROGMEM.
+ *
+ * @param arrayWithStrings A constant array with non-constant strings.
+ * @param amountOfStrings The number of strings in the array.
+ * @param maxSizesOfStringsInPGMArrays An array specifying the maximum number of strings for each individual array.
+ * @param desiredAmountPGMArrays The desired number of program memory arrays.
+ * @param desiredNamingAllUpperCase A string representing the desired naming in all uppercase with underlines as separators.
+ * @param desiredWithIndexArray A flag indicating whether to include index array. If set to 1, index array will be included.
+ *
+ * @note The usage of this converter can be observed in the colossal-cave-adventure repository (located in the /test folder, with the output included in the project).
+ */
+void convertStringsToPGMTextFile(const char ** arrayWithStrings, uint16_t amountOfStrings,
+                                 const uint16_t maxSizesOfStringsInPGMArrays[],
+                                 uint8_t desiredAmountPGMArrays, char * desiredNamingAllUpperCase,
+                                 uint8_t desiredWithIndexArray);
 
 #endif //DWARFOS_PGM_TEXTFILE_GENERATOR_H
